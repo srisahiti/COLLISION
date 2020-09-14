db3387f115bb794136f3d0482bc28a6981611302
srisahiti <71134221+srisahiti@users.noreply.github.com>
2020-09-14 18:49:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# COLLISION
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor="green";
+  fixedRect.debug="true";
+  movingRect=createSprite(400, 200, 80, 30);
+  movingRect.shapeColor="green";
+  movingRect.debug="true";
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2 &&
+    fixedRect.x-movingRect.x < fixedRect.width/2+movingRect.width/2 &&
+    movingRect.y-fixedRect.y < fixedRect.width/2+movingRect.width/2 &&
+    fixedRect.y-movingRect.y < fixedRect.width/2+movingRect.width/2
+    )
+  {
+    fixedRect.shapeColor="red";
+    movingRect.shapeColor="red";
+
+  }
+  else 
+  {
+    fixedRect.shapeColor="green";
+    movingRect.shapeColor="green";
+  }
+  
+  drawSprites();
+}
